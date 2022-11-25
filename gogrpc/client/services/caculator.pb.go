a02c5203df91d3ab41944cdaa5007d1ef7f18e95
CPUser <t-sitthisak.ban@axons.in.th>
2022-11-25 10:39:46 +0700

    unary, server streaming

--- gogrpc/client/services/caculator.pb.go
+++ gogrpc/client/services/caculator.pb.go
@@ -1,8 +1,8 @@
 // build req&res from proto file to server folder (go service)
-// protoc calculator.proto --go_out=../server
+// protoc name.proto --go_out=../server
 
 // build service from proto file to server folder
-// protoc calculator.proto --go-grpc_out=./server
+// protoc name.proto --go-grpc_out=../server
 
 // do the same thing in client
 
@@ -122,6 +122,100 @@ func (x *HelloResponse) GetResult() string {
 	return ""
 }
 
+type FibonacciRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	N uint32 `protobuf:"varint,1,opt,name=n,proto3" json:"n,omitempty"`
+}
+
+func (x *FibonacciRequest) Reset() {
+	*x = FibonacciRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_caculator_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *FibonacciRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FibonacciRequest) ProtoMessage() {}
+
+func (x *FibonacciRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_caculator_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FibonacciRequest.ProtoReflect.Descriptor instead.
+func (*FibonacciRequest) Descriptor() ([]byte, []int) {
+	return file_caculator_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *FibonacciRequest) GetN() uint32 {
+	if x != nil {
+		return x.N
+	}
+	return 0
+}
+
+type FibonacciResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Result uint32 `protobuf:"varint,1,opt,name=result,proto3" json:"result,omitempty"`
+}
+
+func (x *FibonacciResponse) Reset() {
+	*x = FibonacciResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_caculator_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *FibonacciResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FibonacciResponse) ProtoMessage() {}
+
+func (x *FibonacciResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_caculator_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FibonacciResponse.ProtoReflect.Descriptor instead.
+func (*FibonacciResponse) Descriptor() ([]byte, []int) {
+	return file_caculator_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *FibonacciResponse) GetResult() uint32 {
+	if x != nil {
+		return x.Result
+	}
+	return 0
+}
+
 var File_caculator_proto protoreflect.FileDescriptor
 
 var file_caculator_proto_rawDesc = []byte{
@@ -131,13 +225,22 @@ var file_caculator_proto_rawDesc = []byte{
 	0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x22,
 	0x27, 0x0a, 0x0d, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
 	0x12, 0x16, 0x0a, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x32, 0x46, 0x0a, 0x0a, 0x43, 0x61, 0x6c, 0x63,
+	0x52, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x22, 0x20, 0x0a, 0x10, 0x46, 0x69, 0x62, 0x6f,
+	0x6e, 0x61, 0x63, 0x63, 0x69, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x0c, 0x0a, 0x01,
+	0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x01, 0x6e, 0x22, 0x2b, 0x0a, 0x11, 0x46, 0x69,
+	0x62, 0x6f, 0x6e, 0x61, 0x63, 0x63, 0x69, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12,
+	0x16, 0x0a, 0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0d, 0x52,
+	0x06, 0x72, 0x65, 0x73, 0x75, 0x6c, 0x74, 0x32, 0x8e, 0x01, 0x0a, 0x0a, 0x43, 0x61, 0x6c, 0x63,
 	0x75, 0x6c, 0x61, 0x74, 0x6f, 0x72, 0x12, 0x38, 0x0a, 0x05, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x12,
 	0x16, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x73, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f,
 	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x17, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63,
 	0x65, 0x73, 0x2e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
-	0x42, 0x0c, 0x5a, 0x0a, 0x2e, 0x2f, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x73, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x12, 0x46, 0x0a, 0x09, 0x46, 0x69, 0x62, 0x6f, 0x6e, 0x61, 0x63, 0x63, 0x69, 0x12, 0x1a, 0x2e,
+	0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x73, 0x2e, 0x46, 0x69, 0x62, 0x6f, 0x6e, 0x61, 0x63,
+	0x63, 0x69, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x1b, 0x2e, 0x73, 0x65, 0x72, 0x76,
+	0x69, 0x63, 0x65, 0x73, 0x2e, 0x46, 0x69, 0x62, 0x6f, 0x6e, 0x61, 0x63, 0x63, 0x69, 0x52, 0x65,
+	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x30, 0x01, 0x42, 0x0c, 0x5a, 0x0a, 0x2e, 0x2f, 0x73, 0x65,
+	0x72, 0x76, 0x69, 0x63, 0x65, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -152,16 +255,20 @@ func file_caculator_proto_rawDescGZIP() []byte {
 	return file_caculator_proto_rawDescData
 }
 
-var file_caculator_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_caculator_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_caculator_proto_goTypes = []interface{}{
-	(*HelloRequest)(nil),  // 0: services.HelloRequest
-	(*HelloResponse)(nil), // 1: services.HelloResponse
+	(*HelloRequest)(nil),      // 0: services.HelloRequest
+	(*HelloResponse)(nil),     // 1: services.HelloResponse
+	(*FibonacciRequest)(nil),  // 2: services.FibonacciRequest
+	(*FibonacciResponse)(nil), // 3: services.FibonacciResponse
 }
 var file_caculator_proto_depIdxs = []int32{
 	0, // 0: services.Calculator.Hello:input_type -> services.HelloRequest
-	1, // 1: services.Calculator.Hello:output_type -> services.HelloResponse
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	2, // 1: services.Calculator.Fibonacci:input_type -> services.FibonacciRequest
+	1, // 2: services.Calculator.Hello:output_type -> services.HelloResponse
+	3, // 3: services.Calculator.Fibonacci:output_type -> services.FibonacciResponse
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -197,6 +304,30 @@ func file_caculator_proto_init() {
 				return nil
 			}
 		}
+		file_caculator_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*FibonacciRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_caculator_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*FibonacciResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -204,7 +335,7 @@ func file_caculator_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_caculator_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
